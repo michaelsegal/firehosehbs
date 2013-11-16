9c0963bc5fe40155e5e7138e5d9b73a473034b11
Michael Segal <michaelsegal72@gmail.com>
2013-11-16 12:04:55 -0500

    initial rails and bootstrap commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirehoseNew::Application.config.secret_key_base = '8a0e445b6255e32bd29b005839da9508ff50e14fdbf7ccac98470e0624082d7c151921a8f0a2a60e9ab369f455bc99d49d07298726f635f403315c7452aa68e6'
